7eda5172334c15a801bb0c0e55ec445c82e44f6a
jeff-gompertz <139863072+jeff-gompertz@users.noreply.github.com>
2025-11-19 12:27:01 +0700

    Update links in DEFAULT_LINKS array

--- repo-nav-simple.js
+++ repo-nav-simple.js
@@ -1,7 +1,7 @@
 (function(){
   const DEFAULT_LINKS = [
-    { title: '🏠 Index', href: '/CUseeme/index.html', target: '_self' },
-    { title: '📰 Spawn5', href: '/CUseeme/A/CUseeme_v1.7_dynamic_restyleBLACK.html', target: '_self' }
+    { title: '🏠 Index', href: 'index.html', target: '_self' },
+    { title: '📰 Spawn5', href: 'CUseeme_v1.7_dynamic_restyleBLACK.html', target: '_self' }
   ];
 
   if(window.simpleRepoNav) return;
