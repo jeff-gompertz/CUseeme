b170ce708b5677b0a2f098cdec411990d59c5b53
jeff-gompertz <139863072+jeff-gompertz@users.noreply.github.com>
2025-11-20 09:49:18 +0700

    Update hrefs to use leading slashes in links

--- repo-nav-simple.js
+++ repo-nav-simple.js
@@ -9,10 +9,10 @@
   const DEFAULT_LINKS = [
     { title: '📰 Spawn 5', href: 'CUseeme_v1.7_dynamic_restyleBLACK.html', target: '_self' },
     { title: '📰 Spawn 5', href: 'CUseeme_v1.7_dynamic_restyleBLACK.html', target: '_self' },
-    { title: '📰 a Half Half', href: 'CUseeme/a_half_half.html', target: '_self' },
-    { title: '📰 Semantic Voice', href: 'CUseeme/A/A_semantic_voice2.html', target: '_self' },
-    { title: '📰 Floating Feed', href: 'CUseeme/cuseeme_modular_v1.2_floatingfeed.html', target: '_self' },
-    { title: '📰 MultiFilter', href: 'CUseeme/s_6cards_video_wpmedia_multiFilter_LIVEchatgpt2.html', target: '_self' }
+    { title: '📰 a Half Half', href: '/CUseeme/a_half_half.html', target: '_self' },
+    { title: '📰 Semantic Voice', href: '/CUseeme/A/A_semantic_voice2.html', target: '_self' },
+    { title: '📰 Floating Feed', href: '/CUseeme/cuseeme_modular_v1.2_floatingfeed.html', target: '_self' },
+    { title: '📰 MultiFilter', href: '/CUseeme/s_6cards_video_wpmedia_multiFilter_LIVEchatgpt2.html', target: '_self' }
   ];
   
   if (window.simpleRepoNav) return;
